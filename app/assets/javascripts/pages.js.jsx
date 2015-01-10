47bd3489518cf1a302cd29ef947d336bb862d08b
Carlos Lazo <carlos.jose.lazo@gmail.com>
2015-01-10 17:29:47 -0500

    Adds GithubEventListBox and completes MVP. :)

--- app/assets/javascripts/pages.js.jsx
+++ app/assets/javascripts/pages.js.jsx
@@ -18,7 +18,7 @@ var GithubEventList = React.createClass({
 
     var eventNodes = this.props.githubEvents.map(function(githubEvent, index) {
       return (
-        <GithubEvent eventType={githubEvent.eventType} repo={githubEvent.repo} key={index} />
+        <GithubEvent eventType={githubEvent.type} repo={githubEvent.repo.name} key={index} />
       );
     });
 
@@ -31,14 +31,43 @@ var GithubEventList = React.createClass({
 
 });
 
-var ready = function () {
-  var fakeEvents = [
-    { eventType:"IssueEvent", repo:"gvents" },
-    { eventType:"CreateEvent", repo:"rulethejungle" }
-  ];
+var GithubEventListBox = React.createClass({
+  getInitialState: function () {
+    return {githubEvents: []};
+  },
+  componentDidMount: function () {
+    this.loadEventsFromGitHub();
+  },
 
-  React.renderComponent(
-    <GithubEventList githubEvents={fakeEvents} />,
+  loadEventsFromGitHub: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function (githubEvents) {
+        this.setState({githubEvents: githubEvents});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  render: function () {
+    return (
+      <div className="githubEventListBox">
+        <h1>Github Events</h1>
+        <GithubEventList githubEvents={this.state.githubEvents} />
+      </div>
+      );
+  }
+});
+
+var ready = function () {
+  var githubUsername = $("label[name='github-username']").attr('id');
+  var githubUrl      = 'https://api.github.com/users/' + githubUsername + '/events';
+
+  React.render(
+    <GithubEventListBox url={githubUrl} />,
     document.getElementById('github-events')
   );
 };
