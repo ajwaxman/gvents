96ffa6a59a78084bca931052eb84e3f3930d1bd5
Carlos Lazo <carlos.jose.lazo@gmail.com>
2015-01-24 12:47:42 -0500

    Creates parent/child event classes for attribute parsing from JSON.

--- app/assets/javascripts/events.js.jsx
+++ app/assets/javascripts/events.js.jsx
@@ -1,15 +1,59 @@
-var populateGithubEvents = function () {
-  var githubUsername = $("label[name='github-username']").attr('id');
-  var githubUrl      = 'https://api.github.com/users/' + githubUsername + '/events';
+// Event Class
+function Event() {
+  this.repo = ''
+  this.type = ''
+}
 
-  if (githubUsername) {
-    React.render(
-      <GithubEventListBox url={githubUrl} />,
-      document.getElementById('github-events')
-    );
-  }
-};
+Event.prototype.parseJSON = function(json) {
+  this.repo = json.repo.name
+  this.type = json.type
+}
+
+// Create Speific Events
+function PushEvent() {
+  Event.call(this); // Initialize varaibles from parent class.
+  this.commits = 0; // Event specific fields.
+}
+
+PushEvent.prototype = Object.create(Event.prototype);
+PushEvent.prototype.constructor = PushEvent;
+
+PushEvent.prototype.parseJSON = function(json){
+  Event.prototype.parseJSON.call(this,json);
+  this.commits = json.payload.size;
+}
+
+var get_json = function(callback) {
+  $.ajax({
+    url: 'https://api.github.com/users/CarlosPlusPlus/events',
+    dataType: 'json',
+    success: function(response) {
+      callback(response);
+    }
+  });
+}
+
+var parse_json = function(response) {
+  events = []
+  push_event = new PushEvent();
+  push_event.parseJSON(response[1]);
+  events.push(push_event);
+  console.log(events);
+}
+
+// var populateGithubEvents = function () {
+//   var githubUsername = $("label[name='github-username']").attr('id');
+//   var githubUrl      = 'https://api.github.com/users/' + githubUsername + '/events';
+
+//   if (githubUsername) {
+//     React.render(
+//       <GithubEventListBox url={githubUrl} />,
+//       document.getElementById('github-events')
+//     );
+//   }
+// };
 
 $(document).ready(function() {
-  populateGithubEvents();
+  // populateGithubEvents();
+  get_json(parse_json);
 });
